6128d206102d67bed1a100d79ecfc5abb718ccba
Amaimon208 <amaimon208@o2.pl>
2025-05-13 07:01:53 +0200

    Added search to the google map

--- app/src/main/java/com/example/todo/activities/LocationActivity.java
+++ app/src/main/java/com/example/todo/activities/LocationActivity.java
@@ -1,18 +1,29 @@
 package com.example.todo.activities;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.todo.BaseActivity;
 import com.example.todo.R;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.Arrays;
+import java.util.Locale;
 
 public class LocationActivity extends BaseActivity implements OnMapReadyCallback {
 
@@ -29,22 +40,64 @@ public class LocationActivity extends BaseActivity implements OnMapReadyCallback
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+
+
+
+        if (!Places.isInitialized()) {
+            try {
+                ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+                Bundle bundle = ai.metaData;
+                String apiKey = bundle.getString("com.google.android.geo.API_KEY");
+
+                if (!Places.isInitialized()) {
+                    Places.initialize(getApplicationContext(), apiKey, Locale.getDefault());
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                LatLng latLng = place.getLatLng();
+                if (latLng != null) {
+                    mMap.clear();
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(place.getName()));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                    selectedLatLng = latLng;
+                }
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                Toast.makeText(LocationActivity.this, "Błąd: " + status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+
+        // Uniwersytet Zielonogórski - Kampus A
+        LatLng campusA = new LatLng(51.941618, 15.529289);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(campusA, 17)); // 17 = dobre przybliżenie budynku
+        mMap.addMarker(new MarkerOptions().position(campusA).title("UZ - Kampus A"));
+
         mMap.setOnMapClickListener(latLng -> {
-            mMap.clear(); // remove previous marker
-            mMap.addMarker(new MarkerOptions().position(latLng).title("Selected Location"));
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Wybrana lokalizacja"));
             selectedLatLng = latLng;
 
-            LatLng defaultLocation = new LatLng(37.7749, -122.4194); // San Francisco
-            mMap.addMarker(new MarkerOptions().position(defaultLocation).title("Marker"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 12));
-
-            // Return the selected location to ToDoActivity
             Intent resultIntent = new Intent();
             resultIntent.putExtra("lat", latLng.latitude);
             resultIntent.putExtra("lng", latLng.longitude);
